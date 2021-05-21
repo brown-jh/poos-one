ac923efced4158143c01a6d5e91e2e51216a0481
Joseph Brown <brown_jh@knights.ucf.edu>
2021-05-21 14:19:38 -0400

    adapting api

---
+++ LAMPAPI/Login.php
@@ -0,0 +1,61 @@
+<?php
+
+    $inData = getRequestInfo();
+    $id = 0;
+    $firstName = "";
+    $lastName = "";
+
+    $databaseName = "";
+    $databaseUser = "";
+    $databasePassword = "";
+
+    // Connect to the sqlServer '$databaseName' on 'localhost' with the username '$databaseUser'
+    // and the password '$databasePassword
+
+    $conn = new mysqli("localhost", "$databaseUser",  "$databasePassword", "$databaseName"); 
+
+    // If we have a connection error, let us know.
+    if($conn->connect_error)
+    {
+       returnWithError($conn->connect_error);
+    }
+    // If we don't, let's look for the user
+    else
+    {
+        // Prepare a SQL command to send to the server!
+        // **Currently using what was in the class example, but this may need to change
+        // (but probably not)**
+        $stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
+        // Now let's bind our variables to those ?s in the above line
+        $stmt->bind_param("ss", $inData['login'], $inData['password']);
+        // Send the now prepared command!
+        $stmt->execute();
+        // And see if we had a successful login
+        $result = $stmt->get_result();
+
+        // Now that we have our results, lets return the user if it existed.
+        if( $row = $result->fetch_assoc()  )
+		{
+			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+		}
+		else
+		{
+			returnWithError("No Records Found");
+		}
+
+        // Close the connection
+		$stmt->close();
+		$conn->close();
+    } 
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+
+
+
+
+
+?>
