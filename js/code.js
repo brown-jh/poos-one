bbd11cce59cacb81252938363750e7852d359612
Ctomlinson8 <55448119+Ctomlinson8@users.noreply.github.com>
2021-05-25 17:24:13 -0400

    consolidated javascript
    edited index.html, addContact.html, and Register.html to direct itself to code.js

--- js/code.js
+++ js/code.js
@@ -2,106 +2,114 @@ var urlBase = 'http://cop4331group25.xyz/LAMPAPI';
 var extension = 'php';
 
 
-function searchContacts()
+// ** register.html **
+
+
+function registerUser()
 {
-  // First get the data from the user.
-  var first = document.getElementById("firstSearch").value;
-  var last = document.getElementById("lastSearch").value;
-  var phone = document.getElementById("phoneSearch").value;
-  var email = document.getElementById("emailSearch").value;
+  var userId = 0;
+  var firstName = "";
+  var lastName = "";
+	
+  var login = document.getElementById("registerName").value;
+  var password = document.getElementById("registerPassword").value;
+  //var hash = md5( password );
+	
+  document.getElementById("registerResult").innerHTML = "";
 
-  var result = document.getElementById("searchResult");
-  result.innerHTML = ""; // Clear the login result field.
+  //var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
+  var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
 
-  // TODO: This is where we would submit the request to the server.
-  result.innerHTML = "Contact(s) retrieved.<br>"
-  var jsonPayload = '{"first" : "' + first + '", "last" : "' + last + '", "phone" : "' + phone + '", "email" : "' + email + '", "userID" : "' + '1' + '"}';
-  alert("TODO: Should submit to search:\n" + jsonPayload);
-  
-  // This test data can substitute for the request output.
-  // It's rendered as an array of objects, each with fields for the contact data, and another for the primary key.
-  var dummyContacts = [{first:"William", last:"Tell", phone:"", email:"", primKey:10}, 
-		       {first:"Tommy", last:"Tutone", phone:"8675309", email:"", primKey:15}, 
-		       {first:"Professor", last:"Nelson", phone:"", email:"hnelson@uni.edu", primKey:45}, 
-		       {first:"Jane", last:"Smith", phone:"5555555", email:"jsmith@yahoo.com", primKey:46}];
-
-  // For each entry, I show a contact with the text on the left, and Update and Remove buttons on the right.
-  for (var i = 0; i < dummyContacts.length; i++)
+  // TODO: Do database query instead of this test data to check for a duplicate user.
+  if (login == "test")
   {
-    result.innerHTML += makeContactFloats(dummyContacts[i], i);
-  }
-  // If no results were found, say so.
-  if (dummyContacts.length == 0)
-  {
-    result.innerHTML = "No contacts found.<br>";
-  }
-}
-
-
-// This function generates a piece of text and two buttons in HTML to add to the display.
-function makeContactFloats(entry, index)
-{
-  // First make the contact information.
-  var contactInfo = entry.first + " " + entry.last;
-  if (entry.phone != "")
-  {
-    contactInfo += ", " + entry.phone;
+    document.getElementById("registerResult").innerHTML = "Username is already used.<br>";
+    return;
   }
 
-  if (entry.email != "")
+  var passwdRegex = /^\w{8,}$/; //Matches a string of 8 or more alphanumerics.
+  // Check if the password is valid.
+  if (!passwdRegex.test(password))
   {
-    contactInfo += ", " + entry.email;
+    document.getElementById("registerResult").innerHTML = "Password must be 8 or more numbers, letters, or underscores.";
+    return;
   }
-
-  // Next make the parameter listss for the two onClick functions. Both of them need the primary key (to know what database entry to act on)
-  // and the display index (so that it can update the displayed data without re-fetching), but the Update one also needs the 
-  // full data of the entry (to display it as the default data).
-  var deleteParams = entry.primKey + ", " + index;
-  var updateParams = "'" + entry.first + "', '" + entry.last + "', '" + entry.phone + "', '" + entry.email + "', " + deleteParams;
-
-  // Then we need to give a name to the div to reference it later.
-  var divName = "entryDiv" + index;
-
-  // Now plug all these into the HTML.
-  var fullContact = '<div id="' + divName + '"><p>' + contactInfo + 
-			'</p><span class="fullWidth"><input type=button value="Delete" class="smallButton" onClick="deleteContact(' + 
-			deleteParams + ');"><input type=button value="Edit" class="smallButton" onClick="updateContact(' + updateParams + ');"></span></div>';
-  return fullContact;
+  // TODO: Hash password.
+  //var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
+  var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
+  alert("TODO: Should submit to Register API and make cookie:\n" + jsonPayload);
+  // TODO: Database query to add the new user, and make the cookie.
+  location.href = "mainPage.htm";
 }
 
-
-// Send the user to the Add Contact page when the click the Add Contact button.
-function goToAdd()
+// Go to the Register screen when the user clicks on the Register button.
+function goToLogin()
 {
-  location.href = "addContact.html";
-}
-
-// TODO: This function needs a modal box and database access.
-function updateContact(first, last, phone, email, primKey, index)
-{
-  alert("TODO: Bring up a modal dialog box with default values '" + first + "', '" + last + "', '" + phone + "', '" + email + 
-		"' to update the database entry with key " + primKey + ", and update the div indexed " + index + ".");
-}
-
-// TODO: Remove a contact after warning the user.
-function deleteContact(entryKey, entryInd)
-{
-  if(confirm("Do you want to delete this contact?"))
-  {
-    alert("TODO: Tell the database to delete the entry with key " + entryKey + ".");
-
-    // Remove the entry referred to by setting its display to none.
-    document.getElementById("entryDiv" + entryInd).style.display = "none";
-  }
-}
-
-
-//TODO: Return the user to the login page and clear their cookie.
-function logOut()
-{
-  alert("TODO: Clear the user's cookie.");
   location.href = "index.html";
 }
+
+
+// ** addContact.html **
+
+
+function checkContact()
+{
+  // Get the data the user entered.
+  var first = document.getElementById("firstName").value;
+  var last = document.getElementById("lastName").value;
+  var phone = document.getElementById("phoneNumber").value;
+  var email = document.getElementById("emailAddress").value;
+
+  var result = document.getElementById("addResult");
+  result.innerHTML = ""; // Clear the login result field.
+  
+
+  // Check for invalid data: either missing names, invalid phone, or invalid email.
+  // Since the email and phone are optional, don't check them if empty.
+  if (first == "" || last == "")
+  {
+    result.innerHTML += "Please fill in first and last name. <br>";
+  }
+
+  var phoneRegex = /^\d{7}$|^\d{10}$/; //Matches a string of 7 or 10 digits. 
+  if (phone != "" && !phoneRegex.test(phone))
+  {
+    result.innerHTML += "Phone number must be either 7 or 10 digits (no dashes). <br>";
+  }
+  
+  // Check for a valid email (foo@bar.baz) via regex.
+  var emailRegex = /.+\@.+\..+/; 
+  if (email != "" && !emailRegex.test(email))
+  {
+    result.innerHTML += "Email must be of the form name@site.foo. <br>";
+  }
+
+  //TODO: Also check if the entry is duplicate?
+
+  // If we got no errors, then we can submit the data.
+  if (result.innerHTML == "")
+  {
+    // First clear the textboxes.
+    document.getElementById("firstName").value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById("phoneNumber").value = "";
+    document.getElementById("emailAddress").value = "";
+
+    // TODO: This is where we would submit the request to the server.
+    result.innerHTML = "New contact added."
+    var jsonPayload = '{"first" : "' + first + '", "last" : "' + last + '", "phone" : "' + phone + '", "email" : "' + email + '", "userID" : "' + '1' + '"}';
+    alert("TODO: Should submit:\n" + jsonPayload);
+  }
+}
+
+// Return the user to the main page when they click Cancel.
+function goBack()
+{
+  location.href = "mainPage.htm";
+}
+
+
+// ** index.html **
 
 
 function logInUser()
@@ -131,9 +139,12 @@ function logInUser()
   document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
 }
 
-
 // Go to the Register screen when the user clicks on the Register button.
 function goToRegister()
 {
   location.href = "Register.html";
 }
+
+
+// ** mainPage.html **
+
