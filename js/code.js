b62546cbb5cdb0fbe41bd524008194f6cdddad83
Ctomlinson8 <55448119+Ctomlinson8@users.noreply.github.com>
2021-05-26 00:26:51 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -149,8 +149,8 @@ function logInUser()
 				firstName = jsonObject.firstName;
 				lastName = jsonObject.lastName;
 
-        // Need to first implement cookies, then we can uncomment this.
-				//saveCookie();
+        
+				saveCookie();
 	
 				location.href = "mainPage.htm";
 				
@@ -173,3 +173,45 @@ function goToRegister()
 
 // ** mainPage.html **
 
+// **cookie stuff **
+
+function saveCookie()
+{
+	var minutes = 20;
+	var date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	var data = document.cookie;
+	var splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		var thisOne = splits[i].trim();
+		var tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+}
