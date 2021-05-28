05f70413efeca71a0240113fe2fa0fbf61683420
Ctomlinson8 <55448119+Ctomlinson8@users.noreply.github.com>
2021-05-28 13:51:50 -0400

    delete contact edit

--- LAMPAPI/deleteContact.php
+++ LAMPAPI/deleteContact.php
@@ -1,7 +1,9 @@
 <?php 
+
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
 	$primKey = getRequestInfo();
 	
 	$userId = $inData["userId"];
