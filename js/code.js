b9bd6cd9d1b81d4404fd68e035c429b9fdeede55
AlyxReckahn <84057290+AlyxReckahn@users.noreply.github.com>
2021-06-01 23:55:45 -0400

    Cleaned up code formatting, plus some other stuff
    Hey, I cleaned up the general formatting of the code (mix of tabs and spaces for indents due to copy pasting, so I made it all spaces), changed the Add Contact page so it shouldn't erase the info in the boxes if the contact is invalid (the user may have just made a mistake, and might find it annoying to have to retype it), slightly changed come of the text on the webpage, and added a rounded border to the result divs.
    
    I tried to change the error text parts from red text to a red border, but the border was showing up even when there wasn't any text in it, so I wasn't sure how to make that work.

--- js/code.js
+++ js/code.js
@@ -37,46 +37,48 @@ function registerUser()
     return;
   }
 
+  // If we didn't get an error, we can proceed to registering the new account.  
   var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '", "firstName" : "' + registerFirstName + '", "lastName" : "' + registerLastName + '"}'
   //var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '", "firstName" : "' + registerFirstName + '", "lastName" : "' + registerLastName + '"}'
   var url = urlBase + '/Register.' + extension;
 
   // Connects to server and sends jsonPayload containing user information.
   var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				var jsonObject = JSON.parse( xhr.responseText );
-				userId = jsonObject.id;
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try
+  {
+    xhr.onreadystatechange = function() 
+    {
+      if (this.readyState == 4 && this.status == 200) 
+      {
+        var jsonObject = JSON.parse( xhr.responseText );
+        userId = jsonObject.id;
 		
-				firstName = jsonObject.firstName;
-				lastName = jsonObject.lastName;
+        firstName = jsonObject.firstName;
+        lastName = jsonObject.lastName;
  
-				saveCookie();
+        saveCookie();
 	
-        // Sends user back to main menu where they must log in again
-				location.href = "index.html";
-				
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("registerResult").innerHTML = err.message;
-	}
+        // Send the user back to main menu so they can log in.
+        location.href = "index.html";	
+      }
+    };
+    xhr.send(jsonPayload);
+  }
+  catch(err)
+  {
+    document.getElementById("registerResult").innerHTML = err.message;
+  }
 }
+
 
 // Go to the Register screen when the user clicks on the Register button.
 function goToLogin()
 {
   location.href = "index.html";
 }
+
 
 
 // ** addContact.html **
@@ -92,7 +94,6 @@ function checkContact()
   var result = document.getElementById("addResult");
   result.innerHTML = ""; // Clear the login result field.
   
-
   // Check for invalid data: either missing names, invalid phone, or invalid email.
   // Since the email and phone are optional, don't check them if empty.
   if (first == "" || last == "")
@@ -140,15 +141,13 @@ function checkContact()
     {
       result.innerHTML = err.message;
     }
-    
+    document.getElementById("firstName").value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById("phoneNumber").value = "";
+    document.getElementById("emailAddress").value = "";
   }
-
-  document.getElementById("firstName").value = "";
-  document.getElementById("lastName").value = "";
-  document.getElementById("phoneNumber").value = "";
-  document.getElementById("emailAddress").value = "";
-  
 }
+
 
 // Return the user to the main page when they click Cancel.
 function goBack()
@@ -157,12 +156,11 @@ function goBack()
 }
 
 
-// ** index.html **
 
+// ** index.html **
 
 function logInUser()
 {
-
   userId = 0;
   firstName = "";
   lastName = "";
@@ -172,53 +170,50 @@ function logInUser()
   var hash = md5( password );
 
   var jsonPayload = '{"login" : "' + login + '", "password" : "' + hash + '"}';
-  //var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
-	var url = urlBase + '/Login.' + extension;
+  var url = urlBase + '/Login.' + extension;
 
   // Connects to server and sends jsonPayload containing user information, checking if User/Password
   // combo is correct.
   var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				var jsonObject = JSON.parse( xhr.responseText );
-				userId = jsonObject.id;
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try
+  {
+    xhr.onreadystatechange = function() 
+    {
+      if (this.readyState == 4 && this.status == 200) 
+      {
+        var jsonObject = JSON.parse( xhr.responseText );
+        userId = jsonObject.id;
 		
-				if( userId < 1 )
-				{		
-					document.getElementById("loginResult").innerHTML = "User/Password combination incorrect.";
-					return;
-				}
+        if(userId < 1)
+        {		
+          document.getElementById("loginResult").innerHTML = "User/Password combination incorrect.";
+          return;
+        }
 		
-				firstName = jsonObject.firstName;
-				lastName = jsonObject.lastName;
-
+        firstName = jsonObject.firstName;
+        lastName = jsonObject.lastName;
         document.getElementById("loginResult").innerHTML = "Logged in! Welcome!";
- 
-				saveCookie();
-	
-				location.href = "mainPage.htm";
-				
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-	}
+        saveCookie();
+        location.href = "mainPage.htm";
+      }
+    };
+    xhr.send(jsonPayload);
+  }
+  catch(err)
+  {
+    document.getElementById("loginResult").innerHTML = err.message;
+  }
 }
+
 
 // Go to the Register screen when the user clicks on the Register button.
 function goToRegister()
 {
   location.href = "Register.html";
 }
+
 
 
 // ** mainPage.html **
@@ -229,8 +224,10 @@ function updateName()
   document.getElementById("inner-title").innerHTML = "Hello, " + firstName + "! To search a contact, enter in their credentials and press search. Otherwise, to add a contact, please click the button below."
 }
 
+
 // This is the array used to store the contacts.
 var contactList = [];
+
 
 // Searches through the contacts and returns found contacts for you to edit/remove.
 function searchContacts()
@@ -253,22 +250,22 @@ function searchContacts()
 
   // Connects to server and sends jsonPayload containing user information.
   var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try
+  {
+    xhr.onreadystatechange = function() 
+    {
+      if (this.readyState == 4 && this.status == 200) 
+      {
         result.innerHTML = "Contact(s) retrieved.<br>"
                 
         // Updates the contactList array from database, each array index is a contact.
-				var jsonObject = JSON.parse( xhr.responseText );
-				for( var i=0; i<Object.keys(jsonObject).length; i++ )
-				{
-					contactList[i] = jsonObject[i];
-				}
+        var jsonObject = JSON.parse( xhr.responseText );
+        for(var i = 0; i < Object.keys(jsonObject).length; i++)
+        {
+          contactList[i] = jsonObject[i];
+        }
         
         // If no results were found, say so.
         if (jsonObject.id == 0)
@@ -281,24 +278,23 @@ function searchContacts()
         for (var i = 0; i < Object.keys(jsonObject).length; i++)
         {
           result.innerHTML += makeContactFloats(contactList, i);
-        }
-				
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		result.innerHTML = err.message;
-	}
-	
+        }		
+      }  
+    };
+    xhr.send(jsonPayload);
+  }
+  catch(err)
+  {
+    result.innerHTML = err.message;
+  }
 }
+
 
 // This function dynamically generates some HTML to add to the display. It contains two spans; one shows the contact data,
 // and the other lets the user update the data.
 function makeContactFloats(entries, index)
 {
-  // First make the contact information.
+  // First make the contact information. It looks like "John Smith, 1234567, jsmith@yahoo.com" with the phone and email optional.
   var contactInfo = entries[index].FirstName + " " + entries[index].LastName;
   if (entries[index].PhoneNumber != "")
   {
@@ -327,7 +323,7 @@ function makeContactFloats(entries, index)
   var spanName = "results" + index;
   var paraName = "contactInfo" + index;
 
-  // Now plug all these into the HTML. Yes, it is a doozy @_@.
+  // Now plug all these into the HTML.
   var div1 = '<div id="' + divName1 + '"><p id="' + paraName + '">' + contactInfo + 
 			'</p><span class="fullWidth"><input type=button value="Delete" class="smallButton" onClick="deleteContact(' + 
 			params + ');"><input type=button value="Edit" class="smallButton" onClick="updateContact(' + params + ');"></span></div>';
@@ -342,13 +338,14 @@ index + ');"><input type=button value="Confirm" class="smallButton" onClick="acc
   return div1 + div2;
 }
 
+
 // Send the user to the Add Contact page when they click the Add Contact button.
 function goToAdd()
 {
   location.href = "addContact.html";
 }
 
-// Determines which contact the user is currently updating; -1 is none of them.
+// This var determines which contact the user is currently updating; -1 is none of them.
 var currentlyUpdating = -1;
 
 // Opens up the update interface when the user clicks Update.
@@ -362,16 +359,17 @@ function updateContact(entries, index)
   }
   currentlyUpdating = index;
 
-    // Bring up the update interface by switching which div is displayed.
-    document.getElementById("entryDiv" + index).style.display = "none";
-    document.getElementById("updateDiv" + index).style.display = "block";
+  // Bring up the update interface by switching which div is displayed.
+  document.getElementById("entryDiv" + index).style.display = "none";
+  document.getElementById("updateDiv" + index).style.display = "block";
 
-    // Copy the original data in the contact to the search boxes.
-    document.getElementById("first" + index).value = entries[index].FirstName;
-    document.getElementById("last" + index).value = entries[index].LastName;
-    document.getElementById("phone" + index).value = entries[index].PhoneNumber;
-    document.getElementById("email" + index).value = entries[index].Email;
+  // Copy the original data in the contact to the search boxes.
+  document.getElementById("first" + index).value = entries[index].FirstName;
+  document.getElementById("last" + index).value = entries[index].LastName;
+  document.getElementById("phone" + index).value = entries[index].PhoneNumber;
+  document.getElementById("email" + index).value = entries[index].Email;
 }
+
 
 // Get rid of the update interface when the user clicks Cancel.
 function cancelUpdate(index)
@@ -382,7 +380,7 @@ function cancelUpdate(index)
   document.getElementById("updateDiv" + index).style.display = "none";
 }
 
-// Update an entry when the user clicks Accept in the update interface.
+
 // Update an entry when the user clicks Accept in the update interface.
 function acceptUpdate(entries, index)
 {
@@ -396,7 +394,7 @@ function acceptUpdate(entries, index)
   result.innerHTML = ""; // Clear the update result field.
 
   // Check for invalid data: either missing names, invalid phone, or invalid email.
-  // Since the email and phone are optional, don't check them if empty.
+  // Since the email and phone are optional, only check them if not empty.
   if (first == "" || last == "")
   {
     result.innerHTML += "Please fill in first and last name. <br>";
@@ -420,7 +418,7 @@ function acceptUpdate(entries, index)
   // If we got no errors, then we can submit the data.
   if (result.innerHTML == "")
   {
-    //This is where we submit the request to the server.
+    // This is where we submit the request to the server.
     var primKey = entries[index].ID;
 
     var jsonPayload = '{"firstName" : "' + first + '", "lastName" : "' + last + '", "email" : "' + email + '", "phoneNumber" : "' + phone + '", "ID" : "' + primKey + '"}';
@@ -437,26 +435,26 @@ function acceptUpdate(entries, index)
       {
         if (this.readyState == 4 && this.status == 200) 
         {
-            // Update the local version of the entry. First we make the new contact info.
-            var contactInfo = first + " " + last;
-            if (phone != "")
-            {
-              contactInfo += ", " + phone;
-            }
+          // Update the local version of the entry. First we make the new contact info.
+          var contactInfo = first + " " + last;
+          if (phone != "")
+          {
+            contactInfo += ", " + phone;
+          }
 
-            if (email != "")
-            {
-              contactInfo += ", " + email;
-            }
+          if (email != "")
+          {
+            contactInfo += ", " + email;
+          }
 
-            // Then we find the paragraph where the contact info goes and replace it.
-            document.getElementById("contactInfo" + index).innerHTML = contactInfo;
+          // Then we find the paragraph where the contact info goes and replace it.
+          document.getElementById("contactInfo" + index).innerHTML = contactInfo;
 
-            // Replace the data in the array as well, for the next update.
-            entries[index].first = first;
-            entries[index].last = last;
-            entries[index].phone = phone;
-            entries[index].email = email;
+          // Replace the data in the array as well, for the next update.
+          entries[index].first = first;
+          entries[index].last = last;
+          entries[index].phone = phone;
+          entries[index].email = email;
         }
       };
       xhr.send(jsonPayload);
@@ -472,6 +470,7 @@ function acceptUpdate(entries, index)
     document.getElementById("updateDiv" + index).style.display = "none";
   }
 }
+
 
 // Remove a contact after warning the user.
 function deleteContact(entries, index)
@@ -503,11 +502,13 @@ function deleteContact(entries, index)
       result.innerHTML = err.message;
     }
 
-    // Remove the entry referred to by setting its display to none.
+    // Remove the entry on the page by setting its display to none.
     document.getElementById("entryDiv" + index).style.display = "none";
   }
 }
 
+
+// When the user logs out, clear their cookie and direct them to the login page.
 function logOut()
 {
   userId = 0;
@@ -518,41 +519,43 @@ function logOut()
 }
 
 
+
 // **cookie stuff **
 
 function saveCookie()
 {
-	var minutes = 20;
-	var date = new Date();
-	date.setTime(date.getTime()+(minutes*60*1000));	
-	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+  var minutes = 20;
+  var date = new Date();
+  date.setTime(date.getTime()+(minutes*60*1000));	
+  document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
 }
+
 
 function readCookie()
 {
-	userId = -1;
-	var data = document.cookie;
-	var splits = data.split(",");
-	for(var i = 0; i < splits.length; i++) 
-	{
-		var thisOne = splits[i].trim();
-		var tokens = thisOne.split("=");
-		if( tokens[0] == "firstName" )
-		{
-			firstName = tokens[1];
-		}
-		else if( tokens[0] == "lastName" )
-		{
-			lastName = tokens[1];
-		}
-		else if( tokens[0] == "userId" )
-		{
-			userId = parseInt( tokens[1].trim() );
-		}
-	}
+  userId = -1;
+  var data = document.cookie;
+  var splits = data.split(",");
+  for(var i = 0; i < splits.length; i++) 
+  {
+    var thisOne = splits[i].trim();
+    var tokens = thisOne.split("=");
+    if(tokens[0] == "firstName")
+    {
+      firstName = tokens[1];
+    }
+    else if(tokens[0] == "lastName")
+    {
+      lastName = tokens[1];
+    }
+    else if(tokens[0] == "userId")
+    {
+      userId = parseInt( tokens[1].trim() );
+    }
+  }
 	
-	if( userId < 0 )
-	{
-		window.location.href = "index.html";
-	}
+  if(userId < 0)
+  {
+    window.location.href = "index.html";
+  }
 }
